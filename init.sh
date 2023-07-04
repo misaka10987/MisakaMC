34d403cb43b3d20c9e356f1806cbbc00e2c898d8
misaka10987 <misaka10987@outlook.com>
2023-07-04 19:44:07 +0800

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-mkdir -p "./.cache" "./mods" "./config"
-touch "./.cache/mods-server.tar" "./.cache/config.tar" ""
+mkdir -p "${HOME}/.cache/MisakaMC" "./mods" "./config"
+touch "${HOME}/.cache/MisakaMC/mods-server.tar" "${HOME}/.cache/MisakaMC/config.tar" ""
 src="https://github.com/misaka10987/MisakaMC/releases/download/download"
-wget "${src}/config.tar" -O "./.cache/config.tar"
-wget "${src}/mods.tar" -O "./.cache/mods-server.tar"
-tar -xvf "./.cache/config.tar" -C "./config"
-tar -xvf "./.cache/mods-server.tar" -C "./mods"
+wget "${src}/config.tar" -O "${HOME}/.cache/MisakaMC/config.tar"
+wget "${src}/mods.tar" -O "${HOME}/.cache/MisakaMC/mods-server.tar"
+tar -xvf "${HOME}/.cache/MisakaMC/config.tar" -C "./config"
+tar -xvf "${HOME}/.cache/MisakaMC/mods-server.tar" -C "./mods"
