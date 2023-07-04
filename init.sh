93079bd1cd4392c026def988f743a95c1011d2f0
misaka10987 <misaka10987@outlook.com>
2023-07-04 12:39:17 +0800

    Modified README

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-mkdir -p "./.cache" "./mods" "./config" "./world"
-touch "./.cache/mods.tar" "./.cache/config.tar" ""
-wget "https://github.com/misaka10987/MisakaMC/releases/download/download/config.tar" -O "./.cache/config.tar"
-wget "https://github.com/misaka10987/MisakaMC/releases/download/download/mods.tar" -O "./.cache/mods.tar"
-wget "https://github.com/misaka10987/MisakaMC/releases/download/download/save.zip" -O "./.cache/save.zip"
+mkdir -p "./.cache" "./mods" "./config"
+touch "./.cache/mods-server.tar" "./.cache/config.tar" ""
+src="https://github.com/misaka10987/MisakaMC/releases/download/download"
+wget "${src}/config.tar" -O "./.cache/config.tar"
+wget "${src}/mods.tar" -O "./.cache/mods-server.tar"
 tar -xvf "./.cache/config.tar" -C "./config"
-tar -xvf "./.cache/mods.tar" -C "./mods"
-unzip "./.cache/save.zip" -d "./world"
+tar -xvf "./.cache/mods-server.tar" -C "./mods"
